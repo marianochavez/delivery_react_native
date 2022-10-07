30554d153aef6e4b6290509dbe7da281db97e8b8
Mariano Chavez <amarianochavez@gmail.com>
2022-10-07 10:27:36 -0300

    created sanity backend

---
+++ sanity.js
@@ -0,0 +1,17 @@
+import sanityClient from '@sanity/client';
+import { ImageUrlBuilder } from '@sanity/image-url/lib/types/builder';
+
+const client = sanityClient({
+  projectId: '13ojyrag',
+  dataset: 'production',
+  useCdn: true,
+  apiVersion: '2021-10-21',
+});
+
+const builder = ImageUrlBuilder(client);
+export const urlFor = (source) => builder.image(source);
+
+// RUN THIS to add exception for localhost 3000 CORS policy
+// sanity cors add http://localhost:3000
+
+export default client;
